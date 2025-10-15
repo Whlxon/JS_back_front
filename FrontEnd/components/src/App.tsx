b98ab04e34c2181eb1e6af87a29652493e5fbcc3
cyril-houppertz-vinci <cyril.houppertz@student.vinci.be>
2025-10-15 08:41:09 +0200

    New Ex: 2.2

--- FrontEnd/components/src/App.tsx
+++ FrontEnd/components/src/App.tsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+  const pageTitle = "Informations sur les films dans les cinémas";
 
-function App() {
-  const [count, setCount] = useState(0)
+  const cinema1Name = "UGC DeBrouckère";
+  const cinema1Movie1Title = "Film 1 - DeBrouckère";
+  const cinema1Movie1Director = "Director A";
+  const cinema1Movie2Title = "Film 2 - DeBrouckère";
+  const cinema1Movie2Director = "Director B";
+
+  const cinema2Name = "UGC Toison d'Or";
+  const cinema2Movie1Title = "Film 1 - Toison d'Or";
+  const cinema2Movie1Director = "Director C";
+  const cinema2Movie2Title = "Film 2 - Toison d'Or";
+  const cinema2Movie2Director = "Director D";
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Number of click: {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <PageTitle title={pageTitle} />
 
-export default App
+      <Cinema
+        name={cinema1Name}
+        movie1Title={cinema1Movie1Title}
+        director1={cinema1Movie1Director}
+        movie2Title={cinema1Movie2Title}
+        director2={cinema1Movie2Director}
+      />
+
+      <Cinema
+        name={cinema2Name}
+        movie1Title={cinema2Movie1Title}
+        director1={cinema2Movie1Director}
+        movie2Title={cinema2Movie2Title}
+        director2={cinema2Movie2Director}
+      />
+    </div>
+  );
+};
+
+const PageTitle = (page: { title: String }) => {
+  return (
+    <header>
+      <h1>{page.title}</h1>
+    </header>
+  );
+};
+
+const Cinema = (cinema: {
+  name: String;
+  movie1Title: String;
+  director1: String;
+  movie2Title: String;
+  director2: String;
+}) => {
+  return (
+    <header>
+      <h2>{cinema.name}:</h2>
+      <h3>
+        {cinema.movie1Title}
+        <br />
+        de {cinema.director1}
+      </h3>
+      <h3>
+        {cinema.movie2Title}
+        <br />
+        de {cinema.director2}
+      </h3>
+      <br />
+    </header>
+  );
+};
+
+export default App;
