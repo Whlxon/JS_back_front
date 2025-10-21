d304cfcd4fde590f171072b742dbaa06b8cd5053
cyril-houppertz-vinci <cyril.houppertz@student.vinci.be>
2025-10-21 23:41:10 +0200

    new: ex2.3b

--- FrontEnd/ex: 2.1-2-3/ex2.3b/components/src/main.tsx
+++ FrontEnd/ex: 2.1-2-3/ex2.3b/components/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './components/App/App.tsx'
+import App from './components/Main/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
