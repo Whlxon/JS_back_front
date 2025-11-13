fe9e620c953f335d7115c8d72b89e0d050844291
Whlxon <cyril.houppertz@gmaiL.com>
2025-11-13 15:18:48 +0100

    new:ex2.13

---
+++ FrontEnd/ex2.13-14/components/src/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [blague, setBlague] = useState();
+
+  const handleJoke = () => {
+    fetch("https://v2.jokeapi.dev/joke/Any?lang=fr")
+    .then((response) => {
+      if(!response.ok) throw new Error(`<JokeAPI Calling Error>: ${response} : ${response.statusText}`);
+      return response.json();
+    })
+    .then((data) => {setBlague(data); console.log(data)})
+    .catch((err) => {
+      console.log("<JokeAPI Calling Error>: " + err);
+    })
+  }
+
+  useEffect(() => {
+  })
+
+  return (
+    <>
+      {blague && <h1>{blague?.setup}</h1>}
+      {blague && <h2>{blague?.delivery}</h2>}
+      <br />
+      {blague && <h3>Catégorie: {blague?.category}</h3>}
+
+      <div className="card">
+        <button onClick={() => {handleJoke()}}>
+          Faire apparaitre une blague
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
